e7d1d93332a38635ecfa740a95036852be7a2ade
Clayton Allen <20195689+MrChafeits@users.noreply.github.com>
2019-11-05 13:24:03 -0800

    Fixed test script for python quines

--- python/test.sh
+++ python/test.sh
@@ -3,6 +3,7 @@ diff Colleen.py <(./Colleen.py)
 ./Grace.py; diff Grace.py Grace_kid.py
 ./Sully.py
 for i in {4..0}; do
-    chmod +x Sully_4.py
+    chmod +x "Sully_${i}.py"
     diff Sully.py "Sully_${i}.py"
-    chmod +x ./Sully
+    ./"Sully_${i}.py"
+done
